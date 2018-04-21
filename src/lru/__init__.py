c37ef556f70603a05e0256e103f6a42c8c709134
Pavel Goltsev <p.goltsev@gmail.com>
2018-04-21 23:11:34 +0300

    Implement the first non-optimized version of LRU cache decorator
    Tests were added for the decorator.

--- src/lru/__init__.py
+++ src/lru/__init__.py
@@ -2,6 +2,7 @@
 
 Implementation of LRU cache mechanism.
 """
+import sys
 
 
 def lru_cache(max_size=None):
@@ -15,23 +16,102 @@ def lru_cache(max_size=None):
     """
     default_max_size = 100
 
+    class CacheObj(object):
+        __slots__ = ('age', 'key', 'value')
+
+        def __init__(self, age, key, value):
+            super(CacheObj, self).__init__()
+
+            self.age = age
+            self.key = key
+            self.value = value
+
+    class LruCache(object):
+        __slots__ = ('_age_cnt', '_storage', '_size')
+
+        def __init__(self, cache_size):
+            super(LruCache, self).__init__()
+
+            self._size = cache_size
+            self._age_cnt = 0
+            self._storage = {}
+
+        def __setitem__(self, key, value):
+            next_age = self._get_next_age()
+
+            if key in self._storage:
+                cache_obj = self._storage[key]
+                cache_obj.value = value
+                cache_obj.age = next_age
+            else:
+                cache_obj = CacheObj(age=next_age, key=key, value=value)
+                if len(self._storage) < self._size:
+                    self._storage[key] = cache_obj
+                else:
+                    lru_item_key = self._find_lru_item_key()
+                    del self._storage[lru_item_key]
+                    self._storage[key] = cache_obj
+
+        def _get_next_age(self):
+            self._age_cnt += 1
+            return self._age_cnt
+
+        def _find_lru_item_key(self):
+            min_age = sys.maxsize
+            lru_key = None
+            for key, cache_obj in self._storage.items():
+                if min_age > cache_obj.age:
+                    min_age = cache_obj.age
+                    lru_key = key
+            return lru_key
+
+        def __contains__(self, key):
+            return key in self._storage
+
+        def __getitem__(self, key):
+            """Get
+
+            :param args:
+            :return:
+            """
+            if key in self._storage:
+                cache_obj = self._storage[key]
+                cache_obj.age = self._get_next_age()
+                return self._storage[key].value
+
+            raise KeyError
+
+    def get_key_from_args(*args, **kwargs):
+        return args + tuple(sorted(kwargs.items()))
+
     if callable(max_size):
         # if decorator is applied without parameters
         # the first argument will be a function
         func = max_size
-        max_size = default_max_size
+        cache = LruCache(default_max_size)
 
         def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
+            cache_key = get_key_from_args(*args, **kwargs)
+            if cache_key in cache:
+                return cache[cache_key]
+            else:
+                value = func(*args, **kwargs)
+                cache[cache_key] = value
+                return value
 
         return wrapper
     else:
-        if max_size is None:
-            max_size = default_max_size
+        cache = LruCache(default_max_size if max_size is None else max_size)
 
         def decorator(func):
             def wrapper(*args, **kwargs):
-                return func(*args, **kwargs)
+                cache_key = get_key_from_args(*args, **kwargs)
+                if cache_key in cache:
+                    return cache[cache_key]
+                else:
+                    value = func(*args, **kwargs)
+                    cache[cache_key] = value
+                    return value
 
             return wrapper
 
--- src/lru/tests.py
+++ src/lru/tests.py
@@ -1,14 +1,16 @@
 """Tests for LRU cache mechanism."""
 from unittest import TestCase
 
+from mock import mock
+
 from lru import lru_cache
 
 
 class TestLruCache(TestCase):
     """Tests for LRU cache mechanism."""
 
-    def test_cache_with_default_size(self):
-        """Test cache without specifying size."""
+    def test_decorator_itself_with_default_size(self):
+        """Test decoration without specifying size."""
         @lru_cache
         def func(p1, p2):
             return p1 + p2
@@ -21,10 +23,51 @@ class TestLruCache(TestCase):
 
         self.assertEqual(func(1, 2), 3)
 
-    def test_cache_with_given_size(self):
-        """Test cache without specifying size."""
+    def test_decorator_itself_with_given_size(self):
+        """Test decoration without specifying size."""
         @lru_cache(200)
         def func(p1, p2):
             return p1 + p2
 
         self.assertEqual(func(1, 2), 3)
+
+    def test_cache_works(self):
+        """Test that cache works for any combinations of arguments."""
+        func_mock = mock.Mock(return_value=1)
+        func_decorated = lru_cache(func_mock)
+
+        func_decorated(1, 2, param1=1, param2=2, param3=3)
+        func_decorated(1, 2, param3=3, param2=2, param1=1)
+
+        func_mock.assert_called_once_with(1, 2, param1=1, param2=2, param3=3)
+
+    def test_lru(self):
+        """Test that LRU mechanism works correctly."""
+        func_mock = mock.Mock(return_value=1)
+        func = lru_cache(2)(func_mock)
+
+        func(1)  # call function and put value in cache
+        # cache has 1, cache is filled on half
+        func(2)  # call function and put value in cache
+        # cache has 1, 2, cache is full
+        func(2)  # get from cache, not calling the function
+        func(3)  # call function and put value in cache, removing 1
+        # cache has 2, 3
+        func(3)  # get from cache, not calling the function
+        func(4)  # call function and put value in cache, removing 2
+        # cache has 3, 4
+        func(1)  # call function and put value in cache, removing 3
+        # cache has 4, 1
+        func(4)  # get from cache, not calling the function
+        func(1)  # get from cache, not calling the function
+        func(2)  # call function and put value in cache, removing 4
+        # cache has 1, 2
+
+        self.assertEqual(func_mock.mock_calls, [
+            mock.call(1),
+            mock.call(2),
+            mock.call(3),
+            mock.call(4),
+            mock.call(1),
+            mock.call(2),
+        ])
