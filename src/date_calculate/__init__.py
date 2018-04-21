92e9ec2cd3533323992ca3387bbfc54c956e92c0
Pavel Goltsev <p.goltsev@gmail.com>
2018-04-21 14:52:17 +0300

    Add newline to separate from previous variable

--- src/date_calculate/__init__.py
+++ src/date_calculate/__init__.py
@@ -24,6 +24,7 @@ def get_next_lottery_date(date=None):
     # 0 - Mon, 1 - Tue, 2 - Wed, 3 - Thu, 4 - Fri, 5 - Sat, 6 - Sun
     # and time
     LotteryDay = namedtuple('LotteryDay', ('weekday', 'time'))
+
     # store weekdays and time when lottery draw takes place
     # this parameter can be passed as a function argument which
     # gives more flexibility
