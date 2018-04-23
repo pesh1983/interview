2b81c12fa49c59bdc98e54207c9f9635e4841e90
Pavel Goltsev <p.goltsev@gmail.com>
2018-04-23 18:28:50 +0300

    Update docstring of method 'read_dictionary'

--- src/find_anagrams/profiling/__init__.py
+++ src/find_anagrams/profiling/__init__.py
@@ -4,6 +4,8 @@ import os
 
 def read_dictionary():
     """Read words from a file 'dictionary.txt' and return a list of words.
+    The file contains around 479k words. It is obtained from
+    https://github.com/dwyl/english-words and is used as sample data.
 
     :return: A list of words read from a file.
     """
