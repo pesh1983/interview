57da8b5122b347184c740355e4c68a001a88f451
Pavel Goltsev <p.goltsev@gmail.com>
2018-04-23 17:50:30 +0300

    Move 'prepare_storage_with_words' outside of a function
    It is done for better profiling.

--- src/find_anagrams/__init__.py
+++ src/find_anagrams/__init__.py
@@ -15,35 +15,36 @@ def find_anagrams(word, words):
     the given word.
     :return: A list of anagrams if any found.
     """
-    def prepare_storage_with_words():
-        """Prepare a storage with given words for fast lookup.
-
-        It gets given list of words and generates the following structure:
-
-        {
-            <amount of letters in words>: {
-                <sorted sequence of letters of words>: [
-                    <index of a word with these parameters in given list>,
-                    ...
-                ]
-                ...
-            }
-            ...
-        }
-        :return: The structure described above.
-        """
-        result = defaultdict(partial(defaultdict, list))
-        for idx, item in enumerate(words):
-            letters_amount = len(item)
-            sorted_letters = ''.join(sorted(item.lower()))
-            result[letters_amount][sorted_letters].append(idx)
-
-        return result
-
     # prepare storage with data related to words
-    storage = prepare_storage_with_words()
+    storage = prepare_storage_with_words(words)
     # get indexes of anagrams related to parameters of given word
     letters_amount = len(word)
     indexes = storage[letters_amount][''.join(sorted(word.lower()))]
     # gather words from original list and return in required format
     return [words[idx] for idx in indexes]
+
+
+def prepare_storage_with_words(words):
+    """Prepare a storage with given words for fast lookup of anagrams.
+
+    It gets given list of words and generates the following structure:
+
+    {
+        <amount of letters in words>: {
+            <sorted sequence of letters of words>: [
+                <index of a word with these parameters in given list>,
+                ...
+            ]
+            ...
+        }
+        ...
+    }
+    :return: The structure described above.
+    """
+    result = defaultdict(partial(defaultdict, list))
+    for idx, item in enumerate(words):
+        letters_amount = len(item)
+        sorted_letters = ''.join(sorted(item.lower()))
+        result[letters_amount][sorted_letters].append(idx)
+
+    return result
