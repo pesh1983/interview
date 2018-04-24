8dbc4ba342165926a746c3f3d685ecfd9313e3b0
Pavel Goltsev <p.goltsev@gmail.com>
2018-04-24 11:01:38 +0300

    Make refactoring to 'lru_cache' decorator
    Now code of a wrapper function is in single place.

--- src/lru/__init__.py
+++ src/lru/__init__.py
@@ -27,12 +27,26 @@ def lru_cache(max_size=None):
         """
         return args + tuple(sorted(kwargs.items()))
 
+    func = None
     if callable(max_size):
         # if decorator is applied without parameters
         # the first argument will be a function
         func = max_size
-        cache = LruCache(default_max_size)
+        max_size = default_max_size
+    else:
+        # if decorator is executed before applied
+        if max_size is None:
+            # if maximum size is not specified
+            # set it to default
+            max_size = default_max_size
 
+    cache = LruCache(max_size)
+
+    def decorator(func):
+        """Decorate a function.
+
+        :param func: A function to be decorated.
+        """
         @wraps(func)
         def wrapper(*args, **kwargs):
             """Wrap a function.
@@ -50,30 +64,5 @@ def lru_cache(max_size=None):
                 return value
 
         return wrapper
-    else:
-        cache = LruCache(default_max_size if max_size is None else max_size)
 
-        def decorator(func):
-            """Decorate a function.
-
-            :param func: A function to be decorated.
-            """
-            @wraps(func)
-            def wrapper(*args, **kwargs):
-                """Wrap a function.
-
-                :param args: Positional arguments for the function.
-                :param kwargs: Keyword arguments for the function.
-                :return: Result of execution of the function.
-                """
-                cache_key = get_key_from_args(*args, **kwargs)
-                if cache_key in cache:
-                    return cache[cache_key]
-                else:
-                    value = func(*args, **kwargs)
-                    cache[cache_key] = value
-                    return value
-
-            return wrapper
-
-        return decorator
+    return decorator(func) if func else decorator
